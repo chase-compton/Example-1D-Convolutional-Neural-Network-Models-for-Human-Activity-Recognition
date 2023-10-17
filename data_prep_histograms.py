d56cfcf9d870d022ecb8d4a5ed14d7564826eca9
chase-compton <cscompton1@crimson.ua.edu>
2023-10-16 23:03:18 -0500

    added initial stuff

---
+++ data_prep_histograms.py
@@ -0,0 +1,77 @@
+# plot distributions
+from numpy import dstack
+from pandas import read_csv
+from keras.utils import to_categorical
+from matplotlib import pyplot
+
+# load a single file as a numpy array
+def load_file(filepath):
+	dataframe = read_csv(filepath, header=None, delim_whitespace=True)
+	return dataframe.values
+
+# load a list of files and return as a 3d numpy array
+def load_group(filenames, prefix=''):
+	loaded = list()
+	for name in filenames:
+		data = load_file(prefix + name)
+		loaded.append(data)
+	# stack group so that features are the 3rd dimension
+	loaded = dstack(loaded)
+	return loaded
+
+# load a dataset group, such as train or test
+def load_dataset_group(group, prefix=''):
+	filepath = prefix + group + '/Inertial Signals/'
+	# load all 9 files as a single array
+	filenames = list()
+	# total acceleration
+	filenames += ['total_acc_x_'+group+'.txt', 'total_acc_y_'+group+'.txt', 'total_acc_z_'+group+'.txt']
+	# body acceleration
+	filenames += ['body_acc_x_'+group+'.txt', 'body_acc_y_'+group+'.txt', 'body_acc_z_'+group+'.txt']
+	# body gyroscope
+	filenames += ['body_gyro_x_'+group+'.txt', 'body_gyro_y_'+group+'.txt', 'body_gyro_z_'+group+'.txt']
+	# load input data
+	X = load_group(filenames, filepath)
+	# load class output
+	y = load_file(prefix + group + '/y_'+group+'.txt')
+	return X, y
+
+# load the dataset, returns train and test X and y elements
+def load_dataset(prefix=''):
+	# load all train
+	trainX, trainy = load_dataset_group('train', prefix + "UCI HAR Dataset/")
+	print(trainX.shape, trainy.shape)
+	# load all test
+	testX, testy = load_dataset_group('test', prefix + "UCI HAR Dataset/")
+	print(testX.shape, testy.shape)
+	# zero-offset class values
+	trainy = trainy - 1
+	testy = testy - 1
+	# one hot encode y
+	trainy = to_categorical(trainy)
+	testy = to_categorical(testy)
+	print(trainX.shape, trainy.shape, testX.shape, testy.shape)
+	return trainX, trainy, testX, testy
+
+# plot a histogram of each variable in the dataset
+def plot_variable_distributions(trainX):
+	# remove overlap
+	cut = int(trainX.shape[1] / 2)
+	longX = trainX[:, -cut:, :]
+	# flatten windows
+	longX = longX.reshape((longX.shape[0] * longX.shape[1], longX.shape[2]))
+	print(longX.shape)
+	pyplot.figure()
+	xaxis = None
+	for i in range(longX.shape[1]):
+		ax = pyplot.subplot(longX.shape[1], 1, i+1, sharex=xaxis)
+		ax.set_xlim(-1, 1)
+		if i == 0:
+			xaxis = ax
+		pyplot.hist(longX[:, i], bins=100)
+	pyplot.show()
+
+# load data
+trainX, trainy, testX, testy = load_dataset()
+# plot histograms
+plot_variable_distributions(trainX)
